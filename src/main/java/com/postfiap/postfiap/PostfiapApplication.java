7ff6baf57d77b9877ef87e5a4f590ae7d97d9efc
gustavovieirarodrigues1 <gustavo.vieira@hendrios.agency>
2026-04-13 21:03:53 -0300

    start

---
+++ src/main/java/com/postfiap/postfiap/PostfiapApplication.java
@@ -0,0 +1,13 @@
+package com.postfiap.postfiap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostfiapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostfiapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/postfiap/postfiap/PostfiapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postfiap.postfiap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostfiapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
