12f5024adbece6b97f4cf08ff654d33b78aee157
erickvieira <erick-vieira-s@outlook.com>
2026-04-20 23:01:46 -0300

    fix: copilot code review

--- src/main/java/br/com/fiap/postech/adapter/input/controller/ServicesController.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.postech.adapter.input.controller;
-
-import br.com.fiap.postech.port.api.ServicesApi;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class ServicesController implements ServicesApi {
-
-}
