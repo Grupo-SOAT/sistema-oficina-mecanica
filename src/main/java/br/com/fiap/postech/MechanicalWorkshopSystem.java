a4d9646ac5d9c16e0556715c0107304945ed67a7
erickvieira <erick-vieira-s@outlook.com>
2026-04-22 16:36:37 -0300

    feat: implementing supplies feature according to openapi + cucumber specs

--- src/main/java/br/com/fiap/postech/MechanicalWorkshopSystem.java
+++ src/main/java/br/com/fiap/postech/MechanicalWorkshopSystem.java
@@ -2,12 +2,12 @@ package br.com.fiap.postech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MechanicalWorkshopSystem {
-
     public static void main(String[] args) {
         SpringApplication.run(MechanicalWorkshopSystem.class, args);
     }
-
 }
