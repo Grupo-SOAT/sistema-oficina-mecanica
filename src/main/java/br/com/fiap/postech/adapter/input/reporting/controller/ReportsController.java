ed6a652b7c4cb9b85f89528e76e7c39ffa46a715
erickvieira <erick-vieira-s@outlook.com>
2026-04-27 11:33:48 -0300

    feat: implementing export csv endpoint

--- src/main/java/br/com/fiap/postech/adapter/input/reporting/controller/ReportsController.java
+++ src/main/java/br/com/fiap/postech/adapter/input/reporting/controller/ReportsController.java
@@ -6,8 +6,12 @@ import br.com.fiap.postech.port.reporting.catalogservice.CatalogServiceReporting
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,8 +23,34 @@ public class ReportsController implements ReportsApi {
     public ResponseEntity<Resource> getServiceAverageTime(Long id) {
         final var data = catalogServiceReportingUseCase.calculateAverageTime(id);
         final var bytes = catalogServiceReportingPort.writePDF(data);
+        final var contentDisposition = "attachment; filename=\"service-" +
+                id +
+                "-average-time" +
+                LocalDateTime.now() +
+                ".pdf\"";
         final var resource = new ByteArrayResource(bytes);
 
-        return ResponseEntity.ok(resource);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition)
+                .contentType(MediaType.APPLICATION_PDF)
+                .body(resource);
     }
+
+    @Override
+    public ResponseEntity<String> getAllServicesAverageTime() {
+        final var data = catalogServiceReportingUseCase.calculateAverageTime();
+        final var csv = catalogServiceReportingPort.writeCSV(data);
+        final var contentDisposition = "attachment; filename=\"services-average-time_" +
+                LocalDateTime.now() +
+                ".csv\"";
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition)
+                .contentType(MediaType.valueOf("text/csv"))
+                .body(csv);
+    }
+
+    // TODO: implementar exception handler de NOT_FOUND
+
+    // TODO: implementar exception handler de NO_CONTENT
 }
