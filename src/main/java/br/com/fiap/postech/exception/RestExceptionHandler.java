a2864d1300727f3937b7ab2dee8968a561b51cd9
Andre <andre.lui.tornice@gmail.com>
2026-04-28 22:01:05 -0300

    feat: adicionando mais excecoes e validacoes rest handler exception

--- src/main/java/br/com/fiap/postech/exception/RestExceptionHandler.java
+++ src/main/java/br/com/fiap/postech/exception/RestExceptionHandler.java
@@ -6,9 +6,11 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import br.com.fiap.postech.adapter.input.api.model.ErrorResponse;
+import br.com.fiap.postech.domain.authentication.exception.SenhaInvalidaException;
 import br.com.fiap.postech.domain.user.exception.IdUsuarioInexistenteException;
 import br.com.fiap.postech.domain.user.exception.NoMatchingUsersException;
 import br.com.fiap.postech.domain.user.exception.SameUsernameException;
+import br.com.fiap.postech.domain.user.exception.UsuarioNaoEncontradoPorUsernameException;
 
 
 @RestControllerAdvice
@@ -34,6 +36,22 @@ public class RestExceptionHandler {
     public ResponseEntity<Object> handleNoMatchingUsersException(NoMatchingUsersException ex) {
         
         var response = new ErrorResponse(400, "USERNAME_NOT_FOUND_PAGINATION" , ex.getMessage());
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(UsuarioNaoEncontradoPorUsernameException.class)
+    public ResponseEntity<Object> handleUsuarioNaoEncontradoPorUsernameException(UsuarioNaoEncontradoPorUsernameException ex) {
+        
+        var response = new ErrorResponse(400, "USERNAME_NOT_FOUND" , ex.getMessage());
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(SenhaInvalidaException.class)
+    public ResponseEntity<Object> handleSenhaInvalidaException(SenhaInvalidaException ex) {
+        
+        var response = new ErrorResponse(400, "SENHA_INVALIDA" , ex.getMessage());
 
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
