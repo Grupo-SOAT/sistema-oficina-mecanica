c8b3008484e2bb38c394a0cff3f680370c89a38b
Leonardo <oziro56@gmai.com>
2026-05-02 18:32:03 -0300

    feat: liberando /actuador no securityConfig

--- src/main/java/br/com/fiap/postech/config/SecurityConfig.java
+++ src/main/java/br/com/fiap/postech/config/SecurityConfig.java
@@ -55,7 +55,8 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
-                                "/swagger-ui.html")
+                                "/swagger-ui.html",
+                                "/actuator/**")
                         .permitAll()
 
                         .requestMatchers(
