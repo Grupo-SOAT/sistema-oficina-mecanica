453e7d4a96fc740fe3dab322dd3cf39831603068
Andre <65464498+andrezitos3@users.noreply.github.com>
2026-05-08 23:26:51 -0300

    fix: Update ci-seed.sql - erro usuarios

--- src/main/resources/db/seed/ci-seed.sql
+++ src/main/resources/db/seed/ci-seed.sql
@@ -1,12 +1,21 @@
--- =========================
--- USERS
--- =========================
 CREATE TABLE IF NOT EXISTS users (
     user_id SERIAL PRIMARY KEY,
-    user_name VARCHAR(100),
-    password VARCHAR(255),
+    user_name VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
     role TEXT[]
 );
 
+TRUNCATE TABLE users RESTART IDENTITY CASCADE;
+
 INSERT INTO users (user_id, user_name, password, role) VALUES
-(1, 'admin', '$2a$10$IETgyptrfhwJ11oTxUZEf.JAW/km7JpgmN8g51oAp4clPB8LOWCPa', ARRAY['ADMIN'])
+(
+    1,
+    'admin',
+    '$2a$10$IETgyptrfhwJ11oTxUZEf.JAW/km7JpgmN8g51oAp4clPB8LOWCPa',
+    ARRAY['ADMIN']
+);
+
+SELECT setval(
+    pg_get_serial_sequence('users', 'user_id'),
+    (SELECT MAX(user_id) FROM users)
+);
