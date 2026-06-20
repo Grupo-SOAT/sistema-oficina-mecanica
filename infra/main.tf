1d7109359367a8f895be38d201ef6bb59af440a8
Andre <andre.lui.tornice@gmail.com>
2026-06-20 18:14:54 -0300

    fix: update context to minikube context oficina-mecanica-cluster

--- infra/main.tf
+++ infra/main.tf
@@ -14,10 +14,22 @@ resource "null_resource" "minikube" {
   }
 }
 
+resource "null_resource" "update_context" {
+  depends_on = [null_resource.minikube]
+
+  provisioner "local-exec" {
+    command = <<EOT
+      minikube update-context --profile=${var.cluster_name}
+    EOT
+  }
+}
+
+
 resource "null_resource" "wait_cluster" {
 
   depends_on = [
-    null_resource.minikube
+    null_resource.minikube,
+    null_resource.update_context
   ]
 
   provisioner "local-exec" {
